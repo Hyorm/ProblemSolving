065d7d7f0a38f80d19c8546f53a28ee2ae0fe4b8
Hyorm <hyorm1226@gmail.com>
2021-02-17 22:54:32 +0900

    add find rankSearch cpp

--- programmers/kakao2021/rankSearch/rankSearch.cpp
+++ programmers/kakao2021/rankSearch/rankSearch.cpp
@@ -1,29 +1,14 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <algorithm>
 #include <iostream>
 #include <memory.h>
 
-enum type{
-    TYPE_JAVA,
-    TYPE_CPP,
-    TYPE_PYT,
-    
-    TYPE_BE,
-    TYPE_FE,
-    
-    TYPE_JUN,
-    TYPE_SEN,
-    
-    TYPE_CHI,
-    TYPE_PIZ
-};
-
 using namespace std;
+
 void printVector(const vector<int> &vec);
-int findType(string bf);
 vector<int> solution(vector<string> info, vector<string> query);
+bool isDigit(string str);
 
 int main(){
 
@@ -36,107 +21,67 @@ int main(){
 vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
     
-    int** db = new int*[info.size()];
     int db_score[info.size()];
-    
-    for(int i = 0; i < info.size(); i++){
-        db[i] = new int[4];
-        memset(db[i], 0, sizeof(int)*4);
-    }
-    
-    int db_i = 0;
+    int db_id = 0;
     for(const string &iter : info){
-        int db_j = 0;
-        stringstream str(iter);
-        string bf;
-        while(db_j < 5){
-            getline(str, bf, ' ');
-            int tp = findType(bf);
-            if(tp > -2 && db_j < 4){
-				db[db_i][db_j] = tp;
-			}
-			if(tp == -3){
-				db_score[db_i] = stoi(bf);
-			}
-            db_j++;
+        int pos = 0;
+        string tok;
+        string tmp = iter.c_str();
+        string del = " ";
+        while((pos = tmp.find(" ")) != string::npos){
+            tok = tmp.substr(0, pos);
+            tmp.erase(0, pos + del.length());
         }
-        db_i++;
+        db_score[db_id++] = stoi(tmp.substr(0, pos));
     }
+    
     for(const string &iter : query){
-        int count = 0;
-        stringstream str(iter);
-        string bf;
-        int tmp[4];
-        
+        int pos = 0;
+        string tok;
+        string tmp = iter.c_str();
+        string del = " ";
+        string qu_find[4];
+        int qu_id = 0;
         int score = 0;
-        int tmp_i = 0;
-        while(getline(str, bf, ' ')){
-            int tp = findType(bf);
-			if(tp > -2){
-				tmp[tmp_i++] = tp;
-			}
-			if(tp == -3){
-				score = stoi(bf);
-			}
+        while((pos = tmp.find(" ")) != string::npos){
+            tok = tmp.substr(0, pos);
+            if(tok.compare("-") == 0 || tok.compare("and") == 0){
+                tmp.erase(0, pos + del.length());
+				continue;
+            }
+            qu_find[qu_id++] = tok;
+            tmp.erase(0, pos + del.length());
         }
-
-        for(int i = 0; i < info.size(); i++){
-            if(db_score[i] < score){
-                continue;
-            }
-            int check = 0;
-            for(int j = 0; j < 4; j++){
-                if(tmp[j] == -1 || tmp[j] == db[i][j]){
-                    continue;
-                }else{
-                    check++;
-                }
-            }
-            if(check == 0){
-                count++;
-            }
+		
+        score = stoi(tmp.substr(0, pos));
+        int count = 0;
+        int check_id = 0;
+        for(const string &it : info){
+			int check = 0;
+			string in = it.c_str();
+			if(db_score[check_id] >= score){	
+            	for(int i = 0; i < qu_id; i++){
+                	if(in.find(qu_find[i]) != string::npos){
+						check++;
+                	}else{
+						break;
+					}
+            	}
+            	if(check == qu_id){
+                	count++;
+            	}
+			}
+            check_id++;
         }
         answer.push_back(count);
     }
-   
-    for(int i = 0; i < info.size(); i++){
-        delete db[i];
-    }
-    delete[] db;
     
     return answer;
 }
 
-int findType(string bf){
-	int tp = 0;
-	if(bf.compare("java")==0){
-		tp = TYPE_JAVA;
-	}else if(bf.compare("cpp")==0){
-		tp = TYPE_CPP;
-	}else if(bf.compare("python")==0){
-		tp = TYPE_PYT;
-	}else if(bf.compare("backend")==0){
-		tp = TYPE_BE;
-	}else if(bf.compare("frontend")==0){
-		tp = TYPE_FE;
-	}else if(bf.compare("chicken")==0){
-		tp = TYPE_CHI;
-	}else if(bf.compare("pizza")==0){
-		tp = TYPE_PIZ;
-	}else if(bf.compare("junior")==0){
-		tp = TYPE_JUN;
-	}else if(bf.compare("senior")==0){
-		tp = TYPE_SEN;
-	}else if(bf.compare("-")==0){
-        tp = -1;
-    }else if(bf.compare("and")==0 ){
-		tp = -2;
-    }else{
-        tp = -3;
-    }
-	return tp;
+bool isDigit(string str){
+    return atoi(str.c_str()) != 0 || str.compare("0") == 0;
 }
-
 void printVector(const vector<int> &vec) {
     for (const int &it : vec) {
         cout<<it<<" ";
