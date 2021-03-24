ca82da8c8373a033e33eaea2a27af8e309d53eaa
Hyorm <hyorm1226@gmail.com>
2021-03-24 18:20:29 +0900

    LOOOOOOOOOOOOOOOOLLLLLLLLLLLL

--- baekjoon/P2638/P2638.cpp
+++ baekjoon/P2638/P2638.cpp
@@ -1,74 +1,72 @@
 #include <cstdio>
+#include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
 
 int N, M;
-map<int, int> C;
-
+int* MAP;
 bool* check;
 bool out(int cur, int dir);
 int main(){
 	
 	int answer = 0;
-
+	int cc = 0;
 	scanf("%d %d", &N, &M);
-	
+	MAP = new int[N*M];
 	check= new bool[N*M];
-	
 	for(int i = 0; i < N * M; i++){
 		int tmp;
 		cin >> tmp;
-		if(tmp == 1){
-			C.insert(pair<int, int>(i, 0));
-		}
+		if(tmp == 1)cc++;
+		MAP[i] = tmp;
 	}
-	while(!C.empty()){
-		vector<int> R;
-		for(auto it = C.begin();it != C.end(); it++){
-			int ck = 0;
-			int cur = it->first;
-			if(C.find(cur-M)==C.end() && out(cur-M, 0))ck++;
-			if(C.find(cur+M)==C.end() && out(cur+M, 1))ck++;
-			if(C.find(cur+1)==C.end() && out(cur+1, 2))ck++;
-			if(C.find(cur-1)==C.end() && out(cur-1, 3))ck++;
-			if(ck >= 2){
-				R.push_back(cur);
+	while(cc > 0){
+		for(int i = 0; i < N*M; i++){
+			if(MAP[i] == 1){
+				int ck = 0;
+			
+            	if((MAP[i-M]== 0) && out(i-M, 0))ck++;
+            	if((MAP[i+M]== 0) && out(i+M, 1))ck++;
+            	if((MAP[i+1]== 0) && out(i+1, 2))ck++;
+            	if((MAP[i-1]== 0) && out(i-1, 3))ck++;
+				
+				if(ck >= 2){
+					MAP[i] = 2;
+				}
 			}
 
 		}
 		answer++;
-		for(const int &it: R){
-			check[it] = true;
-			C.erase(it);
+		for(int i = 0; i < N*M; i++){
+			if(MAP[i]==2){
+				check[i] = true;
+				MAP[i] = 0;
+				cc--;
+			}
 		}
 	}
 	printf("%d\n", answer);
 }
 bool out(int cur, int dir){
-	if(cur/M < 1 || cur/M >= N-1 || cur%M < 1 || cur%M >= M-1){
+	if(cur/M < 1 || cur/M >= N-1 || cur%M < 1 || cur%M >= M-1 ||check[cur]==true){
 		check[cur] = true;
 		return true;
 	}
-	
-	if(check[cur]==true){
-		return true;
-	}
 	else{
-		if((dir != 1)&&((cur-M)/M > -1) && (C.find(cur-M)==C.end())){
+		if((dir != 1)&&((cur-M)/M > -1)&&(MAP[cur-M]==0)){
 			check[cur-M] = out(cur-M, 0);
 			return check[cur-M];
 		}
-		if((dir != 0)&&((cur+M)/M < N)&&(C.find(cur+M)==C.end())){
+		if((dir != 0)&&((cur+M)/M < N)&&(MAP[cur+M]== 0)){
 			check[cur+M] = out(cur+M, 1);
 			return check[cur+M];
 		}
-		if((dir != 3)&&((cur+1)%M < M)&&(C.find(cur+1)==C.end())){
+		if((dir != 3)&&((cur+1)%M < M)&&(MAP[cur+1]==0)){
 			check[cur+1] = out(cur+1, 2);
 			return check[cur+1];
 		}
-		if((dir != 2)&&((cur-1)%M > -1)&&(C.find(cur-1)==C.end())){
+		if((dir != 2)&&((cur-1)%M > -1)&&(MAP[cur-1]==0)){
 			check[cur-1] = out(cur-1, 3);
 			return check[cur-1];
 		}
