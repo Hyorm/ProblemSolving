b88f6a449f0337a5af9a765943dafd8d50fab701
Kim Hyorim <hyorm1226@gmail.com>
2021-06-05 18:44:23 +0900

    faster

--- baekjoon/algorithm_study/stack/P1406/P1406.cpp
+++ baekjoon/algorithm_study/stack/P1406/P1406.cpp
@@ -1,68 +1,49 @@
-#include <stdio.h>
+#include <cstdio>
 #include <stack>
-#include <iostream>
-#include <string>
 using namespace std;
+int main(){
+    stack<char> left;
+    stack<char> right;
+    char c;
+    int d;
+    while(scanf("%c", &c)!=EOF){
+        if(c!='\n'&&c!=' ')left.push(c);
+        else break;
+    }
+    scanf("%d", &d);
+    while(d-->0){
+        while(scanf("%c", &c)!=EOF)if(c!='\n'&&c!=' ')break;
+        switch(c){
+            case 'L':
+                    if(!left.empty()){
+                        right.push(left.top());
+                        left.pop();
+                    }
+                    break;
+            case 'D':
+                    if(!right.empty()){
+                        left.push(right.top());
+                        right.pop();
+                    }
+                    break;
+            case 'B':
+                    if(!left.empty()){
+                        left.pop();
+                    }
+                    break;
+            case 'P':
+                    while(scanf("%c", &c)!=EOF)if(c!='\n'&&c!=' ')break;
 
-int main(int argc, char** argv){
-
-	stack<char> left;
-	stack<char> right;
-
-	string str;
-
-	cin >> str;
-
-	int N;
-
-	scanf("%d", &N);
-	for(int i = 0 ; i < str.length(); i++){
-		left.push(str.at(i));
-	}
-	for(int i = 0 ; i < N ; i++){
-		char ch ;
-		do{
-			scanf("%c", &ch);
-		}
-		while(!isalpha(ch));
-		switch(ch){
-			case 'L':
-					if(!left.empty()){
-						right.push(left.top());
-						left.pop();
-					}
-					break;
-			case 'D':
-					if(!right.empty()){
-						left.push(right.top());
-						right.pop();
-					}
-					break;
-			case 'B':
-					if(!left.empty()){
-						left.pop();
-					}
-					break;
-			case 'P':
-					char t;
-					do{
-						scanf("%c", &t);
-					}
-					while(!isalpha(t));
-					left.push(t);
-					break;
-
-		}
-	}
-	while(!left.empty()){
-		right.push(left.top());
-		left.pop();
-	}
-
-	while(!right.empty()){
-		printf("%c", right.top());
-		right.pop();
-	}
-	printf("\n");
-
+                    left.push(c);
+                    break;
+        }
+    }
+    while(!left.empty()){
+        right.push(left.top());
+        left.pop();
+    }
+    while(!right.empty()){
+        printf("%c", right.top());
+        right.pop();
+    }
 }
